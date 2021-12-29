2ec63d448fe1f805f872303e502a4846752713ce
Cillian Fogarty <55901988+cillfog1@users.noreply.github.com>
2021-12-29 23:33:15 +0000

    Basic API access

--- src/script.py
+++ src/script.py
@@ -1,1 +1,10 @@
-print("Hello World");
+#import PyGithub library, install using 'pip install PyGithub' 
+from github import Github
+
+g = Github("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")
+
+usr = g.get_user()
+print("user: " + usr.login)
+print("fullname: " + usr.name)
+print("location: " + usr.location)
+print("company: " + usr.company)
