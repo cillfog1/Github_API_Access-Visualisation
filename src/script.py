2e0ed750a85895fed077bc9230c67834c9f1fbeb
Cillian Fogarty <55901988+cillfog1@users.noreply.github.com>
2021-12-29 23:44:32 +0000

    Added basic error handling

--- src/script.py
+++ src/script.py
@@ -5,6 +5,12 @@ g = Github("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")
 
 usr = g.get_user()
 print("user: " + usr.login)
-print("fullname: " + usr.name)
-print("location: " + usr.location)
-print("company: " + usr.company)
+
+if usr.name is not None:
+	print("fullname: " + usr.name)
+
+if usr.location is not None:
+	print("location: " + usr.location)
+
+if usr.company is not None:
+ print("company: " + usr.company)
