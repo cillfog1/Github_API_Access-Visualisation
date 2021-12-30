b6481d93c3b7f6481dc93eb6120c576b033c27ee
Cillian Fogarty <55901988+cillfog1@users.noreply.github.com>
2021-12-30 02:40:03 +0000

    Added code to run docker-compose

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
+docker-compose -f src/docker-compose.yml up -d
 ./gatherData.sh
 ./processData.sh
--- runTest.sh
+++ runTest.sh
@@ -1,3 +1,4 @@
+docker-compose -f src/docker-compose.yml up -d
 python3 test/clearDB.py
 python3 test/sampleDB.py
 ./gatherData.sh
