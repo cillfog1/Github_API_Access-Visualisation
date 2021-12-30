9e89a7642c640e4d3b6902ae5a563d1fa173350d
Cillian Fogarty <55901988+cillfog1@users.noreply.github.com>
2021-12-30 01:15:40 +0000

    Extract data from database

---
+++ src/script2.py
@@ -0,0 +1,35 @@
+#----------------------------- Imports -----------------------------
+#MongoDB access, install using 'pip install pymongo'
+import pymongo
+
+#Pretty print database data
+import pprint
+
+
+#----------------------------- Connect to MongoDB -----------------------------
+#Establish connection
+conn = "mongodb://localhost:27017"
+client = pymongo.MongoClient(conn)
+
+#Check Connection
+try :
+	client.admin.command('ping')
+	#Trace 1: Connected to MongoDB
+	print("Trace 1: Connection to MongoDB established")
+except :
+	#Trace 1.2: Not connected to MongoDB
+	print("Trace 1.2: Connection to MongoDB failed")
+	#Quit if connection cannot be established
+	quit()
+
+
+#Access database 
+db = client.classDB
+
+#Extract the database data
+githubUser = db.githubUser.find()
+
+#Pretty Print data
+for user in githubUser :
+	pprint.pprint(user)
+	print()
