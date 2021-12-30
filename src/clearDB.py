c4ddc38521b919b121cf1475c425d9a0031baa82
Cillian Fogarty <55901988+cillfog1@users.noreply.github.com>
2021-12-30 01:38:58 +0000

    Code to clear the githubUser database

---
+++ src/clearDB.py
@@ -0,0 +1,30 @@
+#----------------------------- Imports -----------------------------
+#MongoDB access, install using 'pip install pymongo'
+import pymongo
+
+
+#----------------------------- Connect to MongoDB -----------------------------
+#Establish connection
+conn = "mongodb://localhost:27017"
+client = pymongo.MongoClient(conn)
+
+#Check Connection
+try :
+	client.admin.command('ping')
+	#Trace 1: Connected to MongoDB
+	print("Trace 1: Connection to MongoDB established")
+except :
+	#Trace 1.2: Not connected to MongoDB
+	print("Trace 1.2: Connection to MongoDB failed")
+	#Quit if connection cannot be established
+	quit()
+
+
+#Access database 
+db = client.classDB
+
+#Clear Database
+db.githubUser.delete_many({})
+
+#Trace 2: githubUser database cleared
+print("Trace 2: githubUser database cleared")
