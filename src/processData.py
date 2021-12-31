c80e52bd7ee91bed4743c9dbc4ebbf0f8a7ed9c6
Cillian Fogarty <55901988+cillfog1@users.noreply.github.com>
2021-12-31 22:16:36 +0000

    Added code to calculate the average commit stats for the other contributers of the repo

--- src/processData.py
+++ src/processData.py
@@ -42,7 +42,7 @@ def extractData(repoName, username) :
 	print("Database : ")
 	#Pretty Print data
 
-	with open('csv/data.csv', 'w') as file :
+	with open('csv/contributerData.csv', 'w') as file :
 		file.write('Date,Additions,Deletions,Total\n')
 		for data in githubData :
 			pprint.pprint(data)
@@ -51,3 +51,53 @@ def extractData(repoName, username) :
 						str(data['additions']) + ',' +
 						str(data['deletions']) + ',' +
 						str(data['total']) + '\n')
+
+	#Average of other members of the team
+	#Extract the database data
+	repoData = reposDB.find({"repoName": repoName})
+	numOfContributers = 0
+	for data in repoData :
+		print("Contributers to repo : " + repoName)
+		for user in data['contributers'] :
+			pprint.pprint(user['username'])
+			print()
+			numOfContributers += 1
+
+	if (numOfContributers > 1) :
+		githubData = commitsDB.find({"repoName": repoName, "username": { "$ne" : username}})
+
+		print("Database : ")
+		#Pretty Print data
+
+		with open('csv/teamData.csv', 'w') as file :
+			file.write('Date,Additions,Deletions,Total\n')
+
+			previousDate = 0
+			totalAdditions = 0
+			totalDeletions = 0
+			totalTotal = 0
+			for data in githubData :
+				pprint.pprint(data)
+				print()
+
+				if (previousDate != data['date'] and previousDate != 0) :
+					file.write(	str(previousDate) + ',' +
+							str(totalAdditions / (numOfContributers - 1)) + ',' +
+							str(totalDeletions / (numOfContributers - 1)) + ',' +
+							str(totalTotal / (numOfContributers - 1)) + '\n')
+					totalAdditions = 0
+					totalDeletions = 0
+					totalTotal = 0
+
+				previousDate = data['date']
+				totalAdditions += data['additions']
+				totalDeletions += data['deletions']
+				totalTotal += data['total']
+			file.write(	str(previousDate) + ',' +
+						str(totalAdditions / (numOfContributers - 1)) + ',' +
+						str(totalDeletions / (numOfContributers - 1)) + ',' +
+						str(totalTotal / (numOfContributers - 1)) + '\n')
+	else :
+		with open('csv/teamData.csv', 'w') as file :
+			file.write('Date,Additions,Deletions,Total\n')
+			file.write(	0 + '\n')
